bfb9297da469af2e952d11a051e9f670900278a7
sebastianflesa <sebastian.orrego@fleischmann.cl>
2024-11-16 22:26:05 -0300

    pruebas unitarias de fecha

--- src/app/registrarse/registrarse.component.spec.ts
+++ src/app/registrarse/registrarse.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegistrarseComponent } from './registrarse.component';
+import { FormsModule, ReactiveFormsModule, AbstractControl, FormControl } from '@angular/forms'; 
 
 describe('RegistrarseComponent', () => {
   let component: RegistrarseComponent;
@@ -8,7 +8,7 @@ describe('RegistrarseComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegistrarseComponent]
+      imports: [RegistrarseComponent, FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
 
@@ -17,7 +17,21 @@ describe('RegistrarseComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('validarEdad', () => {
+    it('retornar null si la edad es mayor o igual a 13 años', () => {
+      const control: AbstractControl = new FormControl('2010-11-14');
+      const result = component.validarEdad(control);
+      expect(result).toBeNull();
+    });
+
+    it('manejar fechas inválidas o vacias', () => {
+      const control: AbstractControl = new FormControl('');
+      const result = component.validarEdad(control);
+      expect(result).toEqual({ ageRestriction: true });
+      const invalidControl: AbstractControl = new FormControl('invalid-date');
+      const resultInvalid = component.validarEdad(invalidControl);
+      expect(resultInvalid).toEqual({ ageRestriction: true });
+    });
   });
+  
 });
--- src/app/registrarse/registrarse.component.ts
+++ src/app/registrarse/registrarse.component.ts
@@ -1,34 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators,AbstractControl,ValidationErrors, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registrarse',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.css'
 })
 export class RegistrarseComponent {
-  nombreCompleto: string = '';
-  nombreUsuario: string = '';
-  correo: string = '';
-  clave: string = '';
-  repetirClave: string = '';
-  fechaNacimiento: string = '';
-  direccion: string = '';
 
-  onSubmit() {
-    if (this.clave !== this.repetirClave) {
-      alert('Las contraseñas no coinciden.');
-      return;
-    }
-    console.log('Datos del formulario:', {
-      nombreCompleto: this.nombreCompleto,
-      nombreUsuario: this.nombreUsuario,
-      correo: this.correo,
-      clave: this.clave,
-      fechaNacimiento: this.fechaNacimiento,
-      direccion: this.direccion
+limpiarFormulario() {
+  this.formularioRegistro.reset();
+  Object.keys(this.formularioRegistro.controls).forEach(field => {
+    const control = this.formularioRegistro.get(field);
+    control?.markAsUntouched({ onlySelf: true });
+  });
+}
+
+submitForm() {
+  
+  if (this.formularioRegistro.invalid) {
+    Object.keys(this.formularioRegistro.controls).forEach(field => {
+      const control = this.formularioRegistro.get(field);
+      control?.markAsTouched({ onlySelf: true });
     });
+    return;
   }
+  console.log(this.formularioRegistro.value);
+
+  
+}
+
+formularioRegistro! : FormGroup;
+
+constructor(private fb: FormBuilder) {
+  this.formularioRegistro = this.fb.group({
+    nombre: ['', Validators.required],
+    nombre_usuario: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(18),
+        Validators.pattern('^(?=.*[0-9])(?=.*[A-Z]).+$')
+      ]
+    ],
+    password2: ['', Validators.required],
+    direccion: [''],
+    fecha_nacimiento: ['', [Validators.required, this.validarEdad]]
+  }, { validator: this.passwordsMatchValidator });
+}
+
+passwordsMatchValidator(group: AbstractControl): ValidationErrors | null {
+  const password = group.get('password')?.value;
+  const password2 = group.get('password2')?.value;
+  return password === password2 ? null : { mismatch: true };
+}
+
+validarEdad(control: AbstractControl): ValidationErrors | null {
+  const birthDate = new Date(control.value);
+  const today = new Date();
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+  const dayDifference = today.getDate() - birthDate.getDate();
+  if (age > 13 || (age === 13 && (monthDifference > 0 || (monthDifference === 0 && dayDifference >= 0)))) {
+    return null;
+  }
+  return { ageRestriction: true };
+}
+
+
 }
