1afc28b3704f3d410c49fd7871d01a6fb8ef44bd
sebastianflesa <sebastian.orrego@fleischmann.cl>
2024-12-01 21:51:06 -0300

    json desde assets como servicio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductosService } from '../productos.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  providers: [ProductosService]
 })
 export class HomeComponent {
+  productos: any;
+
+  constructor(private route: ActivatedRoute, private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.getJsonData().subscribe((data) => {
+      this.productos = data;
+      console.log(this.productos);
+    });
+    
+  }
 
 }
